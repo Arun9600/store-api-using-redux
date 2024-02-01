ec7d60adf28c88cd9e517a15113270f0be2ea30e
Arun <69082994+Arun9600@users.noreply.github.com>
2024-02-01 13:05:04 +0530

    products list updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,43 +1,67 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProductList } from "../features/productsSlice";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography, Button } from "@mui/material";
 const Products = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProductList());
   });
   const { datas } = useSelector((state) => state.productsList);
+
   return (
     <>
       <Box sx={{ padding: "30px 0" }}>
         <Container>
           <Grid container>
-            {datas.map((data) => (
-              <Grid
-                item
-                xl={4}
-                lg={4}
-                md={6}
-                sm={6}
-                xs={12}
-                key={data.id}
-                style={{ marginBottom: "40px", textAlign: "center" }}
-              >
-                <Box>
-                  <img
-                    src={data.image}
-                    alt={data.title}
-                    style={{ width: "200px" }}
-                  />
-                </Box>
-                <Box>
-                  <Typography variant="h6" style={{ fontSize: "18px" }}>
-                    {data.title}
-                  </Typography>
-                </Box>
-              </Grid>
-            ))}
+            {datas &&
+              datas.map((data) => (
+                <Grid
+                  item
+                  xl={4}
+                  lg={4}
+                  md={6}
+                  sm={6}
+                  xs={12}
+                  key={data.id}
+                  style={{ marginBottom: "40px" }}
+                >
+                  <Box>
+                    <img
+                      src={data.image}
+                      alt={data.title}
+                      style={{
+                        height: "300px",
+                        objectFit: "cover",
+                        width: "90%",
+                      }}
+                    />
+                  </Box>
+                  <Box sx={{ padding: "15px" }}>
+                    <Typography variant="h6" style={{ fontSize: "18px" }}>
+                      {data.title}
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      style={{
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        marginBottom: "20px",
+                      }}
+                    >
+                      Rs.{data.price}
+                    </Typography>
+                    <Grid container>
+                      <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
+                        <Button variant="contained">Quick View</Button>
+                      </Grid>
+                      <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
+                        <Button variant="contained">Add To Cart</Button>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </Grid>
+              ))}
           </Grid>
         </Container>
       </Box>
